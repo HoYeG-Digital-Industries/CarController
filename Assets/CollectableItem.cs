eeb50345c91870743ee33831046b6ec167883abc
Istvan Franczuz <ifranczuz@heartofyorkshire.ac.uk>
2025-09-01 15:34:02 +0100

    Tidied up last commit

--- Assets/CollectableItem.cs
+++ Assets/CollectableItem.cs
@@ -6,21 +6,8 @@ public class CollectableItem : MonoBehaviour
 {
     public GameObject CollectedParticleEffectObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     internal void HasBeenCollected()
     {
-        Debug.Log("Collected");
         // Spawn a PFX at this objects position.
         if (CollectedParticleEffectObject is not null)
         {
@@ -30,7 +17,6 @@ public class CollectableItem : MonoBehaviour
             {
                 Destroy(tempGameObject, collectedPFX.startLifetime); // Destroy the PFX when it has finished
             }
-            
         }
 
         // Destroy this GameObject now
